240b3a438dbd16003e03c78f80dbe2490a621e47
cmakihara <clmakihara@gmail.com>
2017-07-01 17:58:34 -0300

    conexao db

---
+++ TrabalhoBimestral/src/main/java/br/casa/ConexaoDB.java
@@ -0,0 +1,54 @@
+package br.casa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class ConexaoDB {
+	private static ConexaoDB self;
+	private Connection con;
+
+	private ConexaoDB() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/trabalho", "postgres", "sa");
+			
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				
+				public void run() {
+					try {
+						ConexaoDB.this.con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}));
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static synchronized ConexaoDB getInstance() {
+		if (self == null) {
+			self = new ConexaoDB();
+		}
+		
+		return self;
+	}
+	
+	public Connection getConnection() {
+		return this.con;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		throw new CloneNotSupportedException("Haver só um");
+	}
+
+}
